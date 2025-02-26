1589ba3bbf991bb3fb05aab915a484e60333f9cb
wuyeelok <wuyeelok@gmail.com>
2025-02-26 08:53:25 +0800

    Batch script is not case sensitive

--- Mood.bat
+++ Mood.bat
@@ -1,9 +1,15 @@
 @echo off
 
+REM Set the mood to Happy and then to Sad
 set myMood=Happy
 echo My mood is %myMood%!
-
 set myMood=Sad
 echo My mood is %myMood%!
+
+
+REM Varaible is case insensitive
+set MYMOOD=cheerful
+echo myMood is %myMood%!
+echo MYMOOD is %MYMOOD%!
 
 pause
