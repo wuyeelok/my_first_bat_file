e502fb87cb22873c3499e284da3eb4202da81636
wuyeelok <wuyeelok@gmail.com>
2025-02-26 09:02:45 +0800

    Ampersand: command separator

--- SpaceAroundAssignmentOper.bat
+++ SpaceAroundAssignmentOper.bat
@@ -2,14 +2,23 @@
 
 REM Set X = Bye, need to watch out common mistake.  > con echo The value of X is %X%
 Set X = Bye
-> con echo The value of X is %X%
+> con echo The value of X is %X%!
 
 REM Set X = Bye > con echo The value of X is %X %
-> con echo The value of X is %X %
+> con echo The value of X is %X %!
 
 REM Set X = Bye > con echo The value of X is % X%
-> con echo The value of X is % X%
+> con echo The value of X is % X%!
 
 REM Set X = Bye > con echo The value of X is % X %
-> con echo The value of X is % X %
+> con echo The value of X is % X %!
+
+REM Set X=Bye& > con echo The value of X is %X%, & is the command separator
+Set X=Bye&
+> con echo The value of X is %X%!
+
+REM Set X=Bye   & > con echo The value of X is %X%!, & is the command separator and can be used to add space in varaible
+Set X=Bye   &
+> con echo The value of X is %X%!
+
 pause
