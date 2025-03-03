6ab391c0eff28b850834d0d4a85df105e8385bc8
wuyeelok <wuyeelok@gmail.com>
2025-03-03 14:42:32 +0800

    setlocal EnableDelayedExpansion demo and difference

--- TryEnableDelayedExpansion.bat
+++ TryEnableDelayedExpansion.bat
@@ -1,12 +1,33 @@
 @echo off
 
-echo "Trying setlocal EnableDelayedExpansion Example 1"
 
 setlocal EnableDelayedExpansion
 set Bike=Giant
 set Giant=Trinity
 
+> con echo "Trying setlocal EnableDelayedExpansion Example 1"
+
 > con echo  Bike = %Bike%& REM Display value of variable Bike
 > con echo Giant = %Giant%& REM Display value of variable Giant
+
+> con echo.& REM Blank line
+> con echo "Trying setlocal EnableDelayedExpansion Example 2"
+
+> con echo Bike = %Bike%& REM Display value of variable Bike normally using percentage
+> con echo Bike = !Bike!& REM Display value of variable Bike using exclamation marks
+
+> con echo Bike = !%Bike%!& REM Display value of variable Bike using percentage surrounded by exclamation marks
+> con echo What had happens is first Bike = ^^!Giant^^!
+> con echo Then Bike = Trinity
+
+endlocal
+
+> con echo "Now no more setlocal EnableDelayedExpansion"
+
+set Bike=Giant
+set Giant=Trinity
+
+> con echo Bike = !%Bike%!& REM Display value of variable Bike using percentage surrounded by exclamation marks without setlocal EnableDelayedExpansion
+> con echo Only one round of conversion is done!
 
 pause
