a7f5418f718674d73626b90aadea437bc86fc76a
wuyeelok <wuyeelok@gmail.com>
2025-03-05 09:14:11 +0800

    setlocal EnableExtensions

--- PartialVariableNames.bat
+++ PartialVariableNames.bat
@@ -1,18 +1,18 @@
 @echo off
 
-setlocal EnableDelayedExpansion
+setlocal EnableExtensions EnableDelayedExpansion
 
 set foodNash=Hot Dog& REM Set variable foodNash to Hot Dog
 set foodNYC=Pizza& REM Set variable foodNYC to Pizza
 set foodLA=Hamburger& REM Set variable foodLA to Hamburger
 
-> con echo  foodNash = %foodNash%, foodNYC = %foodNYC%, foodLA = %foodLA%& REM Display value of variables foodNash, foodNYC, and foodLA
+> con echo foodNash = %foodNash%, foodNYC = %foodNYC%, foodLA = %foodLA%& REM Display value of variables foodNash, foodNYC, and foodLA
 
 set NashFull=Nashville
 set NYCFull=New York City
 set LAFull=Los Angeles
 
-> con echo  NashFull = %NashFull%, NYCFull = %NYCFull%, LAFull = %LAFull%& REM Display value of variables NashFull, NYCFull, and LAFull
+> con echo NashFull = %NashFull%, NYCFull = %NYCFull%, LAFull = %LAFull%& REM Display value of variables NashFull, NYCFull, and LAFull
 
 set city=NYC&
 
