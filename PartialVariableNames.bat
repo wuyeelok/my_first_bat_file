8a82c329c06b61ba618219a732b057f62d007415
wuyeelok <wuyeelok@gmail.com>
2025-03-05 09:03:02 +0800

    Playaround partial variable name example

--- PartialVariableNames.bat
+++ PartialVariableNames.bat
@@ -8,6 +8,16 @@ set foodLA=Hamburger& REM Set variable foodLA to Hamburger
 
 > con echo  foodNash = %foodNash%, foodNYC = %foodNYC%, foodLA = %foodLA%& REM Display value of variables foodNash, foodNYC, and foodLA
 
+set NashFull=Nashville
+set NYCFull=New York City
+set LAFull=Los Angeles
+
+> con echo  NashFull = %NashFull%, NYCFull = %NYCFull%, LAFull = %LAFull%& REM Display value of variables NashFull, NYCFull, and LAFull
+
+set city=NYC&
+
+> con echo The best !food%city%! can only be found in !%city%Full!^^!
+
 endlocal 
 
 pause
